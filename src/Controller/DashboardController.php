80f71944722d4645a205cdf56d67955530970a43
abd <chabane.abdelkarim@worldweb.fr>
2021-05-07 15:04:56 +0200

    on dashboard add button publish and unpublish

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -17,11 +17,11 @@ class DashboardController extends AbstractController
     use LanguageTrait;
 
     /**
-     * @Route("partner/dashboard", name="dashboard")
+     * @Route("partner/dashboard/{id_module}/{state}", name="dashboard")
      * @param Request $request
      * @return Response
      */
-    public function index(Request $request): Response
+    public function index(Request $request, string $id_module = null, bool $state = null): Response
     {
 
         // get the current language
@@ -34,6 +34,21 @@ class DashboardController extends AbstractController
 
         $lmRepo = $this->getDoctrine()->getRepository(LearningModule::class);
         $fm = new FlaggingManager($languageCount);
+
+        if( $id_module !== null && $state !== null  ){
+            $manager = $this->getDoctrine()->getManager();
+            // get the module with specifique id
+            $module = $this->getDoctrine()->getRepository(LearningModule::class)->findOneBy(['id' => $id_module]);
+            // active or disable the module
+            if ($state == 1){
+                $module->setIsPublished($state);
+            } else{
+                $module->setIsPublished(0);
+            }
+            $manager->persist($module);
+            $manager->flush();
+        
+        }
 
         return $this->render('dashboard/index.html.twig', [
             'allModules' => $allModules,
--- src/Controller/DashboardModuleController.php
+++ src/Controller/DashboardModuleController.php
@@ -40,4 +40,6 @@ class DashboardModuleController extends AbstractController
             'languagecount' => $languageCount,
         ]);
     }
+    
+
 }
