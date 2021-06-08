f7aa95cdfc7da35dfd704ca690e60f4a57308f2a
abd <chabane.abdelkarim@worldweb.fr>
2021-06-08 11:15:03 +0200

    add ck editor for quizz

--- src/Form/QuizQuestionTranslationType.php
+++ src/Form/QuizQuestionTranslationType.php
@@ -8,14 +8,15 @@ use App\Entity\QuizQuestionTranslation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class QuizQuestionTranslationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', null, [
-                'attr' => ['placeholder' => 'Question'],
+            ->add('title', TextareaType::class, [
+                'attr' => ['placeholder' => 'Question', 'class' => 'js-richtexteditor-minimal'],
                 'label' => false,
                 'required' => false,
                 'empty_data' => '',
