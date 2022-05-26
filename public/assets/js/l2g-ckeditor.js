467a43eced0bfa73ba4bfc5da32f5b9731d32c28
abd <abd@MacBook-Pro-de-abdel.local>
2022-05-26 12:48:11 +0200

    change html element render by ckeditor for media

--- public/assets/js/l2g-ckeditor.js
+++ public/assets/js/l2g-ckeditor.js
@@ -37,6 +37,43 @@ ClassicEditor.create(document.querySelector('.js-richtexteditor'), {
     image: {
         toolbar: ['imageTextAlternative', 'imageStyle:full', 'imageStyle:side']
     },
+    // Configure 'mediaEmbed' with Iframely previews.
+    mediaEmbed: {
+
+
+        // Previews are always enabled if there’s a provider for a URL (below regex catches all URLs)
+        // By default `previewsInData` are disabled, but let’s set it to `false` explicitely to be sure
+        previewsInData: true,
+
+        providers: [
+            {
+                // hint: this is just for previews. Get actual HTML codes by making API calls from your CMS
+                name: 'video', 
+
+                // Match all URLs or just the ones you need:
+                url: /.+/,
+
+                html: match => {
+                    const url = match[ 0 ];
+                    
+                    // alternatively, use &key= instead of &api_key with the MD5 hash of your api_key
+                    // more about it: https://iframely.com/docs/allow-origins
+                           
+
+                    return (
+                        // If you need, set maxwidth and other styles for 'iframely-embed' class - it's yours to customize
+
+                        `<video controls="" style="width: 100%;"  autoplay="" name="media" >`
+                        +
+                        `<source src="${ url }" type="video/mp4">`
+                        +
+                        `</video>` 
+                       
+                    );
+                }
+            }
+        ]
+    },
     licenseKey: ''
 
 }).then(editor => {
